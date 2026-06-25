831aa9d4a667834034fd66a424b29729351cf8f9
Samarth <samarth.chandra07@gmail.com>
2026-06-25 23:19:00 +0530

    rename Brand Guardian references to MVCA

--- ComplianceQAPipeline/backend/src/api/telemetry.py
+++ ComplianceQAPipeline/backend/src/api/telemetry.py
@@ -2,7 +2,7 @@ import os
 import logging
 from azure.monitor.opentelemetry import configure_azure_monitor
 
-logger = logging.getLogger("brand-guardian-telemetry")
+logger = logging.getLogger("mvca-telemetry")
 
 
 def setup_telemetry():
@@ -15,7 +15,7 @@ def setup_telemetry():
     try:
         configure_azure_monitor(
             connection_string=connection_string,
-            logger_name="brand-guardian-tracer"
+            logger_name="mvca-tracer"
         )
         logger.info("Azure Monitor Tracking Enabled & Connected!")
     except Exception as e:
